89e7a691cb8bbc0a8bf57e7247285b7f8455205f
Artur <coolartur7@gmail.com>
2021-05-04 01:58:22 +0300

    Code Refactoring

--- Assets/Scripts/GameData.cs
+++ Assets/Scripts/GameData.cs
@@ -9,9 +9,8 @@ namespace Platformer
 {
     public class GameData
     {
-        // Cached entities. No need to create filters for them
+        // Cached entity. No need to create filters for it
         public EcsEntity playerEntity { get; set; }
-
         public ConfigurationSO configuration;
         public Text coinCounter;
         public GameObject playerWonPanel;
